09eb90dce30487689cabf818105e7be1e38d684b
Krish C Naik <krishnaik06@gmail.com>
2023-03-15 23:03:39 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 ## import ridge regresor model and standard scaler pickle
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
@@ -38,4 +38,4 @@ def predict_datapoint():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
